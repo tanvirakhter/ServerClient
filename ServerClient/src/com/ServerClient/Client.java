d7d451a5810c0b45bcb138b7a4620e0fbf8e7d18
Tanvir Akhter Shakib <tanvirakhtershakib@gmail.com>
2024-02-16 00:48:13 +0000

    Client Class Created (version 1)

--- ServerClient/src/com/ServerClient/Client.java
+++ ServerClient/src/com/ServerClient/Client.java
@@ -1,10 +1,84 @@
 package com.ServerClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    private String serverAddress;
+    private int serverPort;
+    
+    public Client(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+    }
+    
+    public void start() {
+        try {
+            Socket socket = new Socket(serverAddress, serverPort);
+            System.out.println("Connected to server at " + serverAddress + ":" + serverPort);
+            
+            // Start input thread
+            Thread inputThread = new Thread(new InputThread(socket));
+            inputThread.start();
+            
+            // Start output thread
+            Thread outputThread = new Thread(new OutputThread(socket));
+            outputThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static class InputThread implements Runnable {
+        private Socket socket;
+        
+        public InputThread(Socket socket) {
+            this.socket = socket;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Received: " + line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private static class OutputThread implements Runnable {
+        private Socket socket;
+        
+        public OutputThread(Socket socket) {
+            this.socket = socket;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+                String line;
+                while ((line = consoleReader.readLine()) != null) {
+                    writer.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        String serverAddress = "localhost"; 
+        int serverPort = 12345; 
+        Client client = new Client(serverAddress, serverPort);
+        client.start();
+    }
 }
