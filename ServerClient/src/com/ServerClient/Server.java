bdd799e068cdaec7420a52771fad61d84cb41336
Tanvir Akhter Shakib <tanvirakhtershakib@gmail.com>
2024-02-16 00:46:39 +0000

    Server Created

--- ServerClient/src/com/ServerClient/Server.java
+++ ServerClient/src/com/ServerClient/Server.java
@@ -1,10 +1,36 @@
 package com.ServerClient;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+    private int port;
+    
+    public Server(int port) {
+        this.port = port;
+    }
+    
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started on port " + port);
+            
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
-
-	}
-
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        int port = 12345; 
+        Server server = new Server(port);
+        server.start();
+    }
 }
+
